1ac6e99530e7226bc5f8aec632a73bc6c0dcf981
George Luis Costa Ribeiro <georgel.jc98@gmail.com>
2022-03-14 23:33:42 -0300

    Commit tarefa1

---
+++ tarefa1/main.js
@@ -0,0 +1,15 @@
+console.log("Carro")
+var carro = new Veiculo()
+carro.setPlaca("sda-7844")
+carro.setAno(2019)
+carro.exibirDados()
+
+
+console.log("Caminhao")
+var caminhao = new Caminhao("sal-3522", 2008, 3)
+caminhao.exibirDados()
+
+
+console.log("onibus")
+var onibus = new Onibus("jhd8387", 2015, 45)
+onibus.exibirDados()
