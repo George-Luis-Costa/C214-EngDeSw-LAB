176ec8c9dd2291cd42b2b527dc87c10cac1ab7fe
George Luis Costa Ribeiro <georgel.jc98@gmail.com>
2022-10-27 16:58:28 -0300

    link add

--- aulas 8 e 9 (tasks)/src/infrastructure/database.js
+++ aulas 8 e 9 (tasks)/src/infrastructure/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = `link`;
+const uri = `mongodb+srv://adm:adm@cluster0.htck3hy.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose.connect(uri, {
     useNewUrlParser: true,
